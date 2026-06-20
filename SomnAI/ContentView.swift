27df03558d56ee976dd4250ea01bcb462de451ea
Arjan Walia <waliaarjan96@gmail.com>
2026-06-20 03:25:24 -0700

    Initial Commit

---
+++ SomnAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SomnAI
+//
+//  Created by MANDEEP WALIA on 6/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SomnAI/SomnAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomnAIApp.swift
+//  SomnAI
+//
+//  Created by MANDEEP WALIA on 6/20/26.
+//
+
+import SwiftUI
+
+@main
+struct SomnAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
